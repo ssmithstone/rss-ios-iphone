97de591683fdeb85b52e1feeaae0b494cbf338ca
Stephen Smithstone <stephen@smithstone.co.uk>
2013-11-11 21:03:29 +0000

    start of ios iphone version of rss-reader

---
+++ rss-ios-iphone/SSOOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SSOOSAppDelegate.h
+//  rss-ios-iphone
+//
+//  Created by Stephen Smithstone on 11/11/2013.
+//  Copyright (c) 2013 Smithstone OSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSOOSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ rss-ios-iphoneTests/rss_ios_iphoneTests.m
@@ -0,0 +1,34 @@
+//
+//  rss_ios_iphoneTests.m
+//  rss-ios-iphoneTests
+//
+//  Created by Stephen Smithstone on 11/11/2013.
+//  Copyright (c) 2013 Smithstone OSS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface rss_ios_iphoneTests : XCTestCase
+
+@end
+
+@implementation rss_ios_iphoneTests
+
+- (void)setUp
+{
+    [super setUp];
+
+}
+
+- (void)tearDown
+{
+
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTAssertTrue(YES, @"Should Be true");
+}
+
+@end
